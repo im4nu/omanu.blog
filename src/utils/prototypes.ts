59e77d8d6f36cced0f8631c3bb0362ef875ffd22
im4nu <victorx811@gmail.com>
2024-08-28 09:51:12 -0300

    added more 3 prototypes

--- src/utils/prototypes.ts
+++ src/utils/prototypes.ts
@@ -20,7 +20,7 @@ export const prototypesData = [
     development: false,
   },
   {
-    title: "PersonNow",
+    title: "Aplicativo PersonNow",
     description:
       "Aplicativo para conectar personal trainner com aluno de academia.",
     image: "/icons/person-now-logo.svg",
@@ -37,6 +37,46 @@ export const prototypesData = [
     tech: "FIGMA",
     link: "https://www.figma.com/design/qngBR40RBuibALiR0KcO51/Ticket-zone-%7C-Prot%C3%B3tipo?t=AEnSZC1PWwTotJtn-1",
     id: 4,
+    development: false,
+  },
+  {
+    title: "St Doctor",
+    description:
+      "Landing Pages do sistema de gestão de clínicas.",
+    image: "/images/st-doctor.png",
+    tech: "FIGMA",
+    link: "https://www.figma.com/design/H5qaJbZnN6gzsG4wrGNEcz/St-Doctor---Landing-page?node-id=0-1&t=7ri4tiYmVSsEH96Z-1",
+    id: 5,
+    development: false,
+  },
+  {
+    title: "StartTec Soluções",
+    description:
+      "Landing Pages da empresa Starttec soluções em tecnologia.",
+    image: "/icons/startTecLogo.svg",
+    tech: "FIGMA",
+    link: "https://www.figma.com/design/ScBV6EjOO9mZWDPvYdDKSM/StartTec-Lp?node-id=1-4&t=OlFTh95w6jvOMlaY-1",
+    id: 6,
+    development: false,
+  },
+  {
+    title: "ECOPE",
+    description:
+      "Landing page de curso intensivos e pós graduação.",
+    image: "/icons/ecope-logo.svg",
+    tech: "FIGMA",
+    link: 'https://www.figma.com/design/2V0o4rJxZgU42kZlyxxWvQ/ECOPE-novas-p%C3%A1ginas-de-cursos?node-id=0-1&t=NAxFd6jNNVj7IGTN-1',
+    id: 7,
+    development: false,
+  },
+  {
+    title: "Landing page PersonNow",
+    description:
+      "Landing page do personnow aplicativo de conectar personal trainner e academias.",
+    image: "/icons/person-now-logo.svg",
+    tech: "FIGMA",
+    link: 'https://www.figma.com/design/2V0o4rJxZgU42kZlyxxWvQ/ECOPE-novas-p%C3%A1ginas-de-cursos?node-id=0-1&t=NAxFd6jNNVj7IGTN-1',
+    id: 8,
     development: false,
   },
 ];
