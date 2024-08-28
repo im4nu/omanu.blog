4feceafaa4ac6036d5cb56844f49f0dc664ed6f8
im4nu <victorx811@gmail.com>
2024-08-28 11:45:51 -0300

    teste

--- src/components/ui/Wave-background.tsx
+++ src/components/ui/Wave-background.tsx
@@ -106,7 +106,7 @@ export const WavyBackground = ({
   return (
     <div
       className={cn(
-        "min-h-screen flex flex-col items-center justify-center",
+        "min-h-[750px] lg:min-h-[1024px] flex flex-col items-center justify-center",
         containerClassName
       )}
     >
