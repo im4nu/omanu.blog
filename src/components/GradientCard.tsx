9b168ae35d8e182cc3ff91a1f8aab0e7d10730c8
manu <victorx811@gmail.com>
2025-02-12 08:36:13 -0300

    solved gradient card bug

--- src/components/GradientCard.tsx
+++ src/components/GradientCard.tsx
@@ -21,7 +21,7 @@ export function GradientCard({
 }: GradientCardProps) {
   return (
     <Link href={link}>
-      <BackgroundGradient className="rounded-[22px] w-[250px] lg:w-[300px] max-w-sm p-4 sm:p-10 bg-main dark:bg-zinc-900">
+      <BackgroundGradient className="rounded-[22px] w-[250px] lg:w-[300px] max-w-sm p-4 sm:p-10 bg-zinc-900">
         <Image
           src={image}
           alt="Ilustração"
@@ -29,14 +29,12 @@ export function GradientCard({
           width={200}
           className="object-contain w-[200px] h-[200px]"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <p className="text-base sm:text-xl mt-4 mb-2 text-neutral-200">
           {title}
         </p>
 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          {description}
-        </p>
-        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+        <p className="text-sm text-neutral-400">{description}</p>
+        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 mt-4 text-xs font-bold bg-zinc-800">
           <span>
             Ver {category === "projects" ? "Projetos online" : "Protótipos"}{" "}
           </span>
